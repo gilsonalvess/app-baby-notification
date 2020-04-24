6c3dd4256d7e854a85bad77e5f9a7de70a523659
gilsonalves <gilsonalves.developer@gmail.com>
2020-04-24 01:29:13 -0300

    Implementação inicial FirebaseMessaging

--- app/src/main/java/com/example/baby_notification/MainActivity.java
+++ app/src/main/java/com/example/baby_notification/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.baby_notification;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
