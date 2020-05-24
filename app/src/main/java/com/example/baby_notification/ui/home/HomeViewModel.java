2a4f9c15e7a7a5f2bbd2ddd22a49d41c19e3a1de
gilsonalves <gilsonalves.developer@gmail.com>
2020-05-24 15:23:34 -0300

    # Alteração em strings

--- app/src/main/java/com/example/baby_notification/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/baby_notification/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Baby Notification");
     }
 
     public LiveData<String> getText() {
